471fe41bc5c03ae8d1099ee984d24c62c02ddb96
Junhyung Lee <joseph3.lee@gmail.com>
2023-09-01 10:51:19 +0900

    Create Hello Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+        Student s1 = new Student("Junhyung Lee", "22000576");
+        System.out.println(s1.toString());
+    }
+
+}
